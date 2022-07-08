7d16708406fcb1bb8a9c4e4270494e1cca1e7a43
BlackH57 <henri.besancenotel@gmail.com>
2022-07-08 12:26:59 +0200

    Update Population.cs

--- Assets/Scripts/Genetique/Population.cs
+++ Assets/Scripts/Genetique/Population.cs
@@ -19,34 +19,31 @@ public class Population : MonoBehaviour
         for(int i = 0; i<number; i++)
         {
             IAAlphaBeta ia = new IAAlphaBeta();
-            ia.weight = new float[]{rd.NextDouble(), rd.NextDouble(), rd.NextDouble(), rd.NextDouble()};
+            ia.weight = new float[]{UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value};
             population.Add(ia);
         }
     }
 
-    public IAAlphaBeta reproduce(IAAlphaBeta father, IAAlphaBeta mother)
+    public IAAlphaBeta Reproduce(IAAlphaBeta father, IAAlphaBeta mother)
     {
-        new ia = new IAAlphaBeta(); 
+        IAAlphaBeta ia = new IAAlphaBeta(); 
         for(int i = 0; i<4; i++)
         {
-            ia.weight[i] = (rd.Next(0,1)=0)?father.weight[i]:mother.weight[i];
+            ia.weight[i] = (rd.Next(0,1)==0)?father.weight[i]:mother.weight[i];
         }
         return ia;
     }
 
-    public IAAlphaBeta mutation(IAAlphaBeta father)
+    public IAAlphaBeta Mutation(IAAlphaBeta father)
     {
-        new ia = new IAAlphaBeta();
+        IAAlphaBeta ia = new IAAlphaBeta();
 
-        for(int i = 0; then i<(rd.Next(0,5)); i++)
+        for(int i = 0; i<(rd.Next(0,5)); i++)
         {
-            int car = Next(0,4);
-            ia.weight[car] = ia.weight.car *(1+ (rd.NextDouble()-1)/5);
+            int car = rd.Next(0,4);
+            ia.weight[car] = ia.weight[car] *(1+ (UnityEngine.Random.value-1)/5);
         }
+        return ia;
     }
-
-
-
-
 }
 
